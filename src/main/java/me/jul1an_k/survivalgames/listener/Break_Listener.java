0ae8764b4a981add1323af9336de031b51e61e0b
Julian <jul1an_k@elitepvp.ml>
2017-12-30 14:25:53 +0100

    Stuff.

--- src/main/java/me/jul1an_k/survivalgames/listener/Break_Listener.java
+++ src/main/java/me/jul1an_k/survivalgames/listener/Break_Listener.java
@@ -1,5 +1,6 @@
 package me.jul1an_k.survivalgames.listener;
 
+import me.jul1an_k.survivalgames.utils.manager.MessageManager;
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -8,17 +9,23 @@ import org.bukkit.event.block.BlockBreakEvent;
 import me.jul1an_k.survivalgames.SurvivalGames;
 import me.jul1an_k.survivalgames.utils.GameState;
 
+import java.util.List;
+
 public class Break_Listener implements Listener {
-	
+
+	private MessageManager mana = SurvivalGames.getMessageManager();
+
 	@EventHandler
 	public void onBreak(BlockBreakEvent e) {
 		if(!SurvivalGames.getMessageManager().getBoolean("EnableBuilding")) {
 			e.setCancelled(true);
 			return;
 		}
-		
+
+		List<String> allowedToBreak = mana.getMessages("BreakableBlocks");
+
 		if(SurvivalGames.getStatus() == GameState.INGAME | SurvivalGames.getStatus() == GameState.GRACE | e.getPlayer().hasPermission("SG.BreakBlock")) {
-			if(e.getBlock().getType() == Material.LEAVES | e.getBlock().getType() == Material.LEAVES_2 | e.getPlayer().hasPermission("SG.BreakBlock")) {
+			if(allowedToBreak.contains(e.getBlock().getType().name()) | e.getPlayer().hasPermission("SG.BreakBlock")) {
 				e.setCancelled(false);
 			} else {
 				e.setCancelled(true);
